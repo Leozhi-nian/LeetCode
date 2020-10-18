28ee918c0c80b3e82ff35f89f11a63bd9e292bbf
leozhi <leozhi819@gmail.com>
2020-10-18 14:19:26 +0800

    5. 最长回文子串 中心拓展 + 双指针 43ms

--- src/com/leozhi/topic5/Solution01.java
+++ src/com/leozhi/topic5/Solution01.java
@@ -2,13 +2,17 @@ package com.leozhi.topic5;
 
 /**
  * @author leozhi
+ * 中心拓展 + 双指针
  * 通过
+ * 43ms
  */
 public class Solution01 {
     public String longestPalindrome(String s) {
         String res = "";
         for (int i = 0; i < s.length(); i++) {
+            // 字符串长度为奇数
             String s1 = palindrome(s, i, i);
+            // 字符串长度为偶数
             String s2 = palindrome(s, i, i + 1);
             res = res.length() > s1.length() ? res : s1;
             res = res.length() > s2.length() ? res : s2;
@@ -23,6 +27,7 @@ public class Solution01 {
             left--;
             right++;
         }
+        // String的substring()方法获取到的字符串包括左值，不包括右值
         return s.substring(left + 1, right);
     }
 }
