63c73c964a434fbf947a43c2cb1133005826f367
leozhi <leozhi819@gmail.com>
2020-10-19 13:05:33 +0800

    344. 反转字符串 左右指针 1ms

--- src/com/leozhi/topic344/Solution02.java
+++ src/com/leozhi/topic344/Solution02.java
@@ -6,11 +6,11 @@ package com.leozhi.topic344;
  * 通过
  * 1ms
  */
-public class Solution01 {
+public class Solution02 {
     public void reverseString(char[] s) {
         int left = 0, right = s.length - 1;
         char temp;
-        while (left < s.length / 2) {
+        while (left < right) {
             temp = s[left];
             s[left] = s[right];
             s[right] = temp;
