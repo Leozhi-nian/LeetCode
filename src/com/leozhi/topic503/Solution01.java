bea70f21ddf5280490235235bae66a5bf035d814
leozhi <leozhi819@gmail.com>
2020-10-19 22:11:35 +0800

    503. 下一个更大元素 II 单调栈 16ms

---
+++ src/com/leozhi/topic503/Solution01.java
@@ -0,0 +1,25 @@
+package com.leozhi.topic503;
+
+import java.util.Stack;
+
+/**
+ * @author leozhi
+ * 单调栈
+ * 通过
+ * 16ms
+ */
+public class Solution01 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i % n]) {
+                stack.pop();
+            }
+            res[i % n] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i % n]);
+        }
+        return res;
+    }
+}
