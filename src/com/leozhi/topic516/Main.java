bdd4e63c94f2c40540fad9c1efc42e3776923290
leozhi <leozhi819@gmail.com>
2020-11-02 16:03:28 +0800

    516. 最长回文子序列 动态规划 40ms

---
+++ src/com/leozhi/topic516/Main.java
@@ -0,0 +1,12 @@
+package com.leozhi.topic516;
+
+/**
+ * @author leozhi
+ * 516. 最长回文子序列
+ * 给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/leozhi/topic516/Solution01.java
@@ -0,0 +1,28 @@
+package com.leozhi.topic516;
+
+/**
+ * @author leozhi
+ * 动态规划
+ * 通过
+ * 40ms
+ */
+class Solution01 {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        // base case
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
