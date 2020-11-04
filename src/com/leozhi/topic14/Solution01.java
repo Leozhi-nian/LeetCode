44376be6f7b698451c8b8ec50d1457f23855fd70
leozhi <leozhi819@gmail.com>
2020-11-04 14:21:50 +0800

    14. 最长公共前缀 暴力枚举 1ms

--- src/com/leozhi/topic14/Solution01.java
+++ src/com/leozhi/topic14/Solution01.java
@@ -2,6 +2,9 @@ package com.leozhi.topic14;
 
 /**
  * @author leozhi
+ * 暴力枚举
+ * 通过
+ * 1ms
  */
 public class Solution01 {
     public String longestCommonPrefix(String[] strs) {
@@ -11,13 +14,16 @@ public class Solution01 {
         } else {
             return "";
         }
+        // 获取最短的字符串作为公共前缀
         for (String s : strs) {
             if (s.length() < commonPre.length()) {
                 commonPre = s;
             }
         }
+        // 从公共前缀右侧开始遍历所有字符串相应索引处的字符是否和公共前缀相同
         for (int i = commonPre.length() - 1; i >= 0; i--) {
             for (String str : strs) {
+                // 不同则排除最后一个字符
                 if (str.charAt(i) != commonPre.charAt(i)) {
                     commonPre = commonPre.substring(0, i);
                     break;
