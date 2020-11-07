84daff77b8d3d1b3f7c8ffb71a10449d9dfe44c3
leozhi <leozhi819@gmail.com>
2020-11-07 16:30:38 +0800

    102. 二叉树的层序遍历 广度优先搜索 1ms

---
+++ src/com/leozhi/topic102/Main.java
@@ -0,0 +1,12 @@
+package com.leozhi.topic102;
+
+/**
+ * @author leozhi
+ * 102. 二叉树的层序遍历
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/leozhi/topic102/Solution01.java
@@ -0,0 +1,40 @@
+package com.leozhi.topic102;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author leozhi
+ * 广度优先搜索
+ * 通过
+ * 1ms
+ */
+public class Solution01 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                assert node != null;
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
