b8eff1bb6665b780d1a09af417d902780cf07306
leozhi <leozhi819@gmail.com>
2020-11-12 04:58:45 +0800

    31. 下一个排列 两次扫描 1ms

--- src/com/leozhi/topic31/Solution01.java
+++ src/com/leozhi/topic31/Solution01.java
@@ -4,25 +4,32 @@ import java.util.Arrays;
 
 /**
  * @author leozhi
+ * 两次扫描
+ * 通过
+ * 1ms
  */
-public class Solution01 {
+class Solution01 {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int index = 0;
-        int right = n - 1;
-        for (int left = right - 1; left >=0; left--) {
+        int left = n - 2, right = n - 1;
+        for (; left >= 0; left--, right--) {
             if (nums[right] > nums[left]) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                index = left + 1;
                 break;
             }
-            if (left == 0) {
-                right--;
-                left = right - 1;
+        }
+        if (left < 0) {
+            Arrays.sort(nums);
+            return;
+        }
+        for (int i = n - 1; i > right; i--) {
+            if (nums[i] > nums[left] && nums[i] < nums[right]) {
+                right = i;
+                break;
             }
         }
-        Arrays.sort(nums, index, nums.length);
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        Arrays.sort(nums, left + 1, n);
     }
 }
