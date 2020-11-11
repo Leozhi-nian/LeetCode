042f3001db4022dd5ef8eb1b7f9806ceb57a9a04
leozhi <leozhi819@gmail.com>
2020-11-11 21:05:15 +0800

    107. 二叉树的层次遍历 II 队列 1ms

---
+++ src/com/leozhi/topic107/Main.java
@@ -0,0 +1,12 @@
+package com.leozhi.topic107;
+
+/**
+ * @author leozhi
+ * 107. 二叉树的层次遍历 II
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/leozhi/topic107/Solution01.java
@@ -0,0 +1,37 @@
+package com.leozhi.topic107;
+
+import java.util.*;
+
+/**
+ * @author leozhi
+ * 队列
+ * 通过
+ * 1ms
+ */
+public class Solution01 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
