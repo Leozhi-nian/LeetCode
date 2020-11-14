82dd1f18c699f1c6c374f6852e0ddb20416f9c3e
leozhi <leozhi819@gmail.com>
2020-11-14 10:05:52 +0800

    103. 二叉树的锯齿形层次遍历 广度优先搜索 1ms

---
+++ src/com/leozhi/topic103/Main.java
@@ -0,0 +1,12 @@
+package com.leozhi.topic103;
+
+/**
+ * @author leozhi
+ * 103. 二叉树的锯齿形层次遍历
+ * 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/leozhi/topic103/Solution01.java
@@ -0,0 +1,40 @@
+package com.leozhi.topic103;
+
+import java.util.*;
+
+/**
+ * @author leozhi
+ * 广度优先搜索
+ * 通过 1ms
+ */
+public class Solution01 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            if (depth % 2 == 0) {
+                Collections.reverse(list);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
