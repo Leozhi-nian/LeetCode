d64662d964c7878f239e21daa6c15204afec0ecd
leozhi <leozhi819@gmail.com>
2020-11-18 22:46:19 +0800

    134. 加油站 一次遍历 0ms

---
+++ src/com/leozhi/topic134/Solution01.java
@@ -0,0 +1,34 @@
+package com.leozhi.topic134;
+
+/**
+ * @author leozhi
+ * 一次遍历
+ * 官方题解
+ * 通过
+ * 0ms
+ */
+public class Solution01 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int i = 0;
+        while (i < n) {
+            int sumOfGas = 0, sumOfCost = 0;
+            int cnt = 0;
+            while (cnt < n) {
+                int j = (i + cnt) % n;
+                sumOfGas += gas[j];
+                sumOfCost += cost[j];
+                if (sumOfCost > sumOfGas) {
+                    break;
+                }
+                cnt++;
+            }
+            if (cnt == n) {
+                return i;
+            } else {
+                i = i + cnt + 1;
+            }
+        }
+        return -1;
+    }
+}
