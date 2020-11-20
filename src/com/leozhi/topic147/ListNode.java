be619ced00ff639f0e0d39112a92f61afbe67d7f
leozhi <leozhi819@gmail.com>
2020-11-20 23:22:18 +0800

    147. 对链表进行插入排序 从前往后找插入点 3ms

---
+++ src/com/leozhi/topic147/ListNode.java
@@ -0,0 +1,10 @@
+package com.leozhi.topic147;
+
+/**
+ * @author leozhi
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/com/leozhi/topic147/Solution01.java
@@ -0,0 +1,34 @@
+package com.leozhi.topic147;
+
+/**
+ * @author leozhi
+ * 从前往后找插入点
+ * 官方题解
+ * 通过
+ * 3ms
+ */
+public class Solution01 {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode lastSorted = head, cur = head.next;
+        while (cur != null) {
+            if (lastSorted.val <= cur.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode pre = dummy;
+                while (pre.next.val <= cur.val) {
+                    pre = pre.next;
+                }
+                lastSorted.next = cur.next;
+                cur.next = pre.next;
+                pre.next = cur;
+            }
+            cur = lastSorted.next;
+        }
+        return dummy.next;
+    }
+}
