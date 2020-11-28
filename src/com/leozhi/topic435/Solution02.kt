01a24c710d19e05b31d6539694db77f2842d6601
leozhi <leozhi819@gmail.com>
2020-11-28 11:47:56 +0800

    435. 无重叠区间 贪心算法 Java 4ms 贪心算法 Kotlin 236ms

--- src/com/leozhi/topic435/Solution02.kt
+++ src/com/leozhi/topic435/Solution02.kt
@@ -13,9 +13,12 @@ class Solution02 {
         if (len <= 1) {
             return 0
         }
-        intervals.sortWith(Comparator { o1, o2 ->  o1[1] - o2[1]})
+        // 将所有区间按照终点升序排列
+        intervals.sortWith { o1, o2 -> o1[1] - o2[1] }
+        // 初始化保留区间的最后一个区间为第一个区间
         var pre = intervals[0]
         for (i in 1 until len) {
+            // 如果当前区间的起点小于保留区间中的最后一个区间的中点，则应将当前区间删除，否则将保留区间的最后一个区间更新为当前区间
             if (intervals[i][0] < pre[1]) {
                 res++
             } else {
