570ca9ca1cf666222d7f1a6a210b42a269358924
leozhi <leozhi819@gmail.com>
2020-12-06 08:21:57 +0800

    118. 杨辉三角 数学 1ms

---
+++ src/com/leozhi/topic118/Main.java
@@ -0,0 +1,12 @@
+package com.leozhi.topic118;
+
+/**
+ * @author leozhi
+ * 118. 杨辉三角
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
