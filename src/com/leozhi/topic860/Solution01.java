57102022d9753a2018109c13a218239cc765475f
leozhi <leozhi819@gmail.com>
2020-12-10 10:03:44 +0800

    readme.md

---
+++ src/com/leozhi/topic860/Solution01.java
@@ -0,0 +1,29 @@
+package com.leozhi.topic860;
+
+/**
+ * @author leozhi
+ */
+public class Solution01 {
+    public boolean lemonadeChange(int[] bills) {
+        int[] backnote = new int[2];
+        for (int bill : bills) {
+            if (bill == 5) {
+                ++backnote[0];
+            } else if (bill == 10) {
+                --backnote[0];
+                ++backnote[1];
+            } else if (bill == 20) {
+                --backnote[0];
+                if (backnote[1] > 0) {
+                    --backnote[1];
+                } else {
+                    backnote[0] -= 2;
+                }
+            }
+            if (backnote[0] < 0 || backnote[1] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
