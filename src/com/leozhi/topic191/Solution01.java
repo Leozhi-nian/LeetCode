94a320b7349c02801c9ac6e44c6d219a1bd1c795
leozhi <leozhi819@gmail.com>
2020-12-23 22:27:37 +0800

    191. 位1的个数 bitCount() 1ms

---
+++ src/com/leozhi/topic191/Solution01.java
@@ -0,0 +1,13 @@
+package com.leozhi.topic191;
+
+/**
+ * @author leozhi
+ * bitCount()
+ * 通过
+ * 1ms
+ */
+public class Solution01 {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
