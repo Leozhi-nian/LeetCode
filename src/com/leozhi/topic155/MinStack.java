fced2d584e7045f185aa4ee62955a7fc7c626c6b
leozhi <leozhi819@gmail.com>
2020-12-25 20:27:24 +0800

    155. 最小栈 辅助栈 8ms

---
+++ src/com/leozhi/topic155/MinStack.java
@@ -0,0 +1,39 @@
+package com.leozhi.topic155;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author leozhi
+ * 辅助栈
+ * 通过
+ * 8ms
+ */
+class MinStack {
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
