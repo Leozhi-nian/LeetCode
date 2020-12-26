d759641bf9d9c3005d862f44ff7f302697112615
leozhi <leozhi819@gmail.com>
2020-12-26 11:38:28 +0800

    85. 最大矩形 使用柱状图的优化暴力方法 22ms

---
+++ src/com/leozhi/topic85/Main.java
@@ -0,0 +1,13 @@
+package com.leozhi.topic85;
+
+/**
+ * @author leozhi
+ * 85. 最大矩形
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution01 solution = new Solution01();
+        solution.maximalRectangle(new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}});
+    }
+}
