29f09a5979881bdf6962647590d8868abd62d2c9
leozhi <leozhi819@gmail.com>
2021-01-11 21:24:52 +0800

    1720. 解码异或后的数组 位运算 2ms

---
+++ src/com/leozhi/topic1720/Main.java
@@ -0,0 +1,18 @@
+package com.leozhi.topic1720;
+
+/**
+ * @author leozhi
+ * 1720. 解码异或后的数组
+ * 未知 整数数组 arr 由 n 个非负整数组成。
+ *
+ * 经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。例如，arr = [1,0,2,1] 经编码后得到 encoded = [1,2,3] 。
+ *
+ * 给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。
+ *
+ * 请解码返回原数组 arr 。可以证明答案存在并且是唯一的。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/leozhi/topic1720/Solution01.java
@@ -0,0 +1,20 @@
+package com.leozhi.topic1720;
+
+/**
+ * @author leozhi
+ * 位运算
+ * 通过
+ * 2ms
+ */
+public class Solution01 {
+    public int[] decode(int[] encoded, int first) {
+        int len = encoded.length;
+        int[] arr = new int[len + 1];
+        arr[0] = first;
+        for (int i = 1; i < len; i++) {
+            arr[i] = encoded[i - 1] ^ arr[i - 1];
+        }
+
+        return arr;
+    }
+}
