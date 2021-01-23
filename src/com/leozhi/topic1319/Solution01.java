279bce97a2291a69d2e852b3cf2a1d833c1bcee5
leozhi <leozhi819@gmail.com>
2021-01-23 20:53:17 +0800

    1319. 连通网络的操作次数 并查集 7ms

---
+++ src/com/leozhi/topic1319/Solution01.java
@@ -0,0 +1,72 @@
+package com.leozhi.topic1319;
+
+import java.util.Arrays;
+
+/**
+ * @author leozhi
+ * 并查集
+ * 官方题解
+ * 通过
+ * 7ms
+ */
+class Solution01 {
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) {
+            return -1;
+        }
+
+        UnionFind uf = new UnionFind(n);
+        for (int[] conn : connections) {
+            uf.unite(conn[0], conn[1]);
+        }
+
+        return uf.setCount - 1;
+    }
+}
+
+// 并查集模板
+class UnionFind {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFind(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+}
