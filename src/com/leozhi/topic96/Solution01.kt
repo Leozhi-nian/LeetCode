1b5fabd05349fd3fe6c48e7bef673181a1f5d0f7
leozhi <leozhi819@gmail.com>
2021-02-27 19:53:54 +0800

    96. 不同的二叉搜索树 动态规划 Kotlin 148ms

--- src/com/leozhi/topic96/Solution01.kt
+++ src/com/leozhi/topic96/Solution01.kt
@@ -8,10 +8,6 @@ package com.leozhi.topic96
  */
 class Solution01 {
     fun numTrees(n: Int): Int {
-        // 1
-        // 12                                                21
-        // 312          132             123                  321         213             (231)
-        // 4312 3124    4132 1432 1324  4123 1423 1243 1234  4321 3214   4213 2143 2134
         val dp = Array(n + 1) { 0 }
         dp[0] = 1
         dp[1] = 1
