9e57784986dee027379e1f0f897d8829c440378a
leozhi <leozhi819@gmail.com>
2021-03-30 19:51:17 +0800

    剑指 Offer 15. 二进制中1的个数 位运算 Java 1ms

---
+++ src/com/leozhi/offer15/Main.java
@@ -0,0 +1,12 @@
+package com.leozhi.offer15;
+
+/**
+ * @author leozhi
+ * 剑指 Offer 15. 二进制中1的个数
+ * 
+ * 请实现一个函数，输入一个整数（以二进制串形式），输出该数二进制表示中 1 的个数。例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/leozhi/offer15/Solution01.java
@@ -0,0 +1,18 @@
+package com.leozhi.offer15;
+
+/**
+ * @author leozhi
+ * 位运算
+ * 通过
+ * 1ms
+ */
+public class Solution01 {
+    public int hammingWeight(int n) {
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+        n = n + (n >>> 8);
+        n = n + (n >>> 16);
+        return n & 0x3f;
+    }
+}
