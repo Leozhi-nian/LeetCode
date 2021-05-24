de7b94b6e220d65e317e6baf2b6e62f07eba7b7e
leozhi <leozhi819@vip.qq.com>
2021-05-24 21:02:55 +0800

    664. 奇怪的打印机 动态规划 Java 14ms

---
+++ src/com/leozhi/topic664/Solution01.java
@@ -0,0 +1,30 @@
+package com.leozhi.topic664;
+
+/**
+ * @author leozhi
+ * 动态规划
+ * 通过
+ * 官方题解
+ * 14ms
+ */
+public class Solution01 {
+    public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] f = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            f[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    f[i][j] = f[i][j - 1];
+                } else {
+                    int minn = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        minn = Math.min(minn, f[i][k] + f[k + 1][j]);
+                    }
+                    f[i][j] = minn;
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
+}
