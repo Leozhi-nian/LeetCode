526115fb9f0d5eed3c59d67e5b12f177c43650cc
leozhi <leozhi819@vip.qq.com>
2021-05-25 13:20:06 +0800

    1787. 使所有区间的异或结果为零 动态规划 Java 741ms

---
+++ src/com/leozhi/topic1787/Main.java
@@ -0,0 +1,16 @@
+package com.leozhi.topic1787;
+
+/**
+ * @author leozhi
+ * 1787. 使所有区间的异或结果为零
+ * 给你一个整数数组 nums    和一个整数 k      。区间 [left, right]（left <= right）的 异或结果 是对下标位于 left 和 right（包括 left 和 right ）之间所有元素进行 XOR 运算的结果：nums[left] XOR nums[left+1] XOR ... XOR nums[right] 。
+ *
+ * 返回数组中 要更改的最小元素数 ，以使所有长度为 k 的区间异或结果等于零。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/make-the-xor-of-all-segments-equal-to-zero
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main {
+
+}
